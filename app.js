31e92828e0bdc960a873c16361e6d30af4ad0ce8
Richard Arocutipa Lope <richardreyesarocutipalope@gmail.com>
2023-08-15 21:17:34 -0500

    Add basic menu

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+const { mostrarMenu, pause } = require("./helpers/mensajes");
+
 require("colors");
 
 const main = async () => {
   console.log("Hola mundo");
+
+  let opt = "";
+  do {
+    opt = await mostrarMenu();
+    console.log({ opt });
+    await pause();
+  } while (opt !== "0");
 };
 
 main();
