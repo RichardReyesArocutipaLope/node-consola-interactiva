86f84213c2c2b919155600f5ef5cc9c248d7f9df
Richard Arocutipa Lope <richardreyesarocutipalope@gmail.com>
2023-08-16 00:57:50 -0500

    Add load tasks

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import colors from "colors";
 import { inquirerMenu, leerInput, pausa } from "./helpers/inquirer.js";
 import { Tareas } from "./models/tareas.js";
+import { guardarDB, leerDB } from "./helpers/guardarArchivo.js";
 
 const main = async () => {
   let opt = "";
-
   const tareas = new Tareas();
+  const tareasDB = leerDB();
+
+  if (tareasDB) tareas.cargarTareasFromArray(tareasDB);
 
   do {
     opt = await inquirerMenu();
@@ -15,13 +18,15 @@ const main = async () => {
         tareas.crearTarea(desc);
         break;
       case "2":
-        console.log(tareas._listado);
+        console.log(tareas.listadoArr);
         break;
       case "3":
         break;
       case "4":
         break;
     }
+
+    guardarDB(tareas.listadoArr);
     await pausa();
   } while (opt !== "0");
 };
--- models/tareas.js
+++ models/tareas.js
@@ -3,6 +3,16 @@ import { Tarea } from "./tarea.js";
 export class Tareas {
   _listado = {};
 
+  get listadoArr() {
+    const listado = [];
+
+    Object.keys(this._listado).forEach((key) => {
+      const tarea = this._listado[key];
+      listado.push(tarea);
+    });
+
+    return listado;
+  }
   constructor() {
     this._listado = {};
   }
@@ -10,5 +20,11 @@ export class Tareas {
   crearTarea(desc = "") {
     const tarea = new Tarea(desc);
     this._listado[tarea.id] = tarea;
+  }
+
+  cargarTareasFromArray(tareas = []) {
+    tareas.forEach((tarea) => {
+      this._listado[tarea.id] = tarea;
+    });
   }
 }
